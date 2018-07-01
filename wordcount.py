714889cb0e0fd8dcd2a2d23b96fcc207d003df95
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-01 15:46:59 -0700

    Further Study #1

--- wordcount.py
+++ wordcount.py
@@ -7,6 +7,10 @@ def count_words(filename):
         line = line.rstrip()
         line = line.split(" ")
         for word in line:
+            for letter in word:
+                if not letter.isalpha():
+                    word = word.replace(letter,"")
+            word=word.lower()
             dict_words[word] = dict_words.get(word,0) + 1
 
 
@@ -14,3 +18,4 @@ def count_words(filename):
         print(key, dict_words[key])
 
 count_words("twain.txt")
+
