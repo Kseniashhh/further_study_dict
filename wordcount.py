540ae4eb02f74f9e99366746ad916223b1cf017d
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-01 15:27:50 -0700

    Second draft

--- wordcount.py
+++ wordcount.py
@@ -1,3 +1,13 @@
 filename = open("test.txt")
 
-def
+def count_words(filename):
+    dict_words = {}
+    for line in filename:
+        line = line.rstrip()
+        line = line.split(" ")
+        for word in line:
+            dict_words[word] = dict_words.get(word,0) + 1
+
+
+    for key in dict_words:
+        print(key, dict_words[key])
